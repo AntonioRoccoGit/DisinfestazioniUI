abfa8f06e2cd55687dd17745e1c46b4da52b5ffa
AntonioRoccoGit <antonio.rocco97@gmail.com>
2026-02-21 15:55:14 +0100

    aggiunte immagini jumbo per ogni service view

--- src/data/services.js
+++ src/data/services.js
@@ -1,5 +1,10 @@
 import disinfestazione from '@/assets/servizi/disinfestazioni.webp'
-import derattizzazioneImg from '@/assets/servizi/derattizzazioni.webp'
+import derattizzazioneImg from '@/assets/servizi/derattizzazione.webp'
+import volatiliImg from '@/assets/servizi/allontanamentoVolatili.webp'
+import disinfezioniImg from '@/assets/servizi/disinfezioni.webp'
+import trattamentoLegnoImg from '@/assets/servizi/trattamentoLegno.webp'
+import aziendeImg from '@/assets/servizi/aziende.webp'
+
 import ServiceDerattizzazione from '@/components/serviceviews/ServiceDerattizzazione.vue'
 import ServiceDisinfestazione from '@/components/serviceviews/ServiceDisinfestazione.vue'
 import ServiceVolatili from '@/components/serviceviews/ServiceVolatili.vue'
@@ -33,7 +38,7 @@ export const services = [
         label: 'Allontanamento Volatili',
         title: 'Proteggi i tuoi spazi da contaminazioni e degrado',
         description: 'Sistemi dissuasivi professionali per prevenire la presenza di volatili e ridurre i rischi igienico-sanitari negli ambienti esposti.',
-        heroImage: derattizzazioneImg,
+        heroImage: volatiliImg,
         metaTitle: 'Volatili | Alpha Range',
         metaDescription: 'Servizio professionale di allontanamento volatili.',
         component: markRaw(ServiceVolatili)
@@ -43,7 +48,7 @@ export const services = [
         label: 'Disinfezione & Sanificazione',
         title: 'Protezione concreta per persone, clienti e ambienti',
         description: 'Trattamenti professionali per ridurre la carica microbica e migliorare la qualità degli spazi in cui si vive e si lavora.',
-        heroImage: derattizzazioneImg,
+        heroImage: disinfezioniImg,
         metaTitle: 'Disinfezione & Sanificazione | Alpha Range',
         metaDescription: 'Servizio professionale di sanificazione.',
         component: markRaw(ServiceSanificazione)
@@ -53,7 +58,7 @@ export const services = [
         label: 'Trattamenti del Legno',
         title: 'Protezione duratura per strutture in legno',
         description: 'Interventi professionali per preservare il legno da degrado biologico e prolungarne l’integrità nel tempo.',
-        heroImage: derattizzazioneImg,
+        heroImage: trattamentoLegnoImg,
         metaTitle: 'Trattamenti del Legno | Alpha Range',
         metaDescription: 'Servizio professionale di trattamento del legno.',
         component: markRaw(ServiceTrattamentoLegno)
@@ -62,16 +67,16 @@ export const services = [
         slug: 'aziende',
         label: 'Servizi per Aziende',
         title: 'Servizi Professionali per Aziende',
-        description: 'Interventi professionali per preservare il legno da degrado biologico e prolungarne l’integrità nel tempo.',
-        heroImage: derattizzazioneImg,
+        description: 'Supportiamo aziende, attività commerciali e realtà industriali con servizi professionali di prevenzione, controllo e gestione delle infestazioni.',
+        heroImage: aziendeImg,
         metaTitle: 'Per le Aziende | Alpha Range',
-        metaDescription: 'Servizio professionale di allontanamento volatili.',
+        metaDescription: 'Collaborazione con aziende',
         component: markRaw(ServiceAziende)
     }
 ]
 
 export const servicesRoute = [
-    { label: 'Disinfestazione', route: '/servizi/disidisinfestazione-insettinfestazione' },
+    { label: 'Disinfestazione', route: '/servizi/disinfestazione-insetti' },
     { label: 'Derattizzazione', route: '/servizi/derattizzazione' },
     { label: 'Volatili', route: '/servizi/volatili' },
     { label: 'Sanificazione', route: '/servizi/sanificazione' },
